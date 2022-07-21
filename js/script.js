5aaef3cbde747088b1f65ef7eed2838707beb73e
Parktaehoon1 <92parkth@gmail.com>
2022-07-21 18:03:36 +0900

    비주얼 코드 진행중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 window.onload = function () {
+
+
+    $(window).scroll(function(){
+        let temp = $(window).scrollTop();
+        console.log(temp)
+        let header = $('.header');
+        let headerLogoFix = $('.header-logo > .logo')
+
+
+        if(temp > 0) {
+            header.addClass('header-open');
+            headerLogoFix.addClass('logo-fix');
+        } else {
+            header.removeClass('header-open');
+            headerLogoFix.removeClass('logo-fix');
+
+        }
+    })
+
+
+
+    let visualArray = ["1","2","3","4"];
     new Swiper('.sw-visual', {
         effect : 'fade',
         speed: 1000,
@@ -8,9 +30,12 @@ window.onload = function () {
             disableOnInteraction: false,
         },
         pagination: {
-            el: '.swiper-visual-pg',
-            type: 'progressbar',
-            clickable: true,
+            el: '.swiper-pagination',
+            clickable: 'true',
+            type: 'bullets',
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + '<em>'+ visualArray[index]+'</em>' + '<i></i>' + '<b></b>'  + '</span>';
+              },
         },
         navigation: {
             prevEl: '.sw-visual-prev',
